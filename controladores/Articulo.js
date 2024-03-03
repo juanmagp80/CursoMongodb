128d218b5daccd3c12331f8bf1d37bc113c95dac
Juanma Garrido <juangpdev@gmail.com>
2024-03-03 02:12:29 +0100

    añadido metodo actualizar

--- controladores/Articulo.js
+++ controladores/Articulo.js
@@ -67,32 +67,120 @@ const crear = async (req, res) => {
 
 // Devolver una respuesta
 
-const uno = (req, res) => {
-  // Recoger un id por la url
-  let id = req.params.id;
+const uno = async (req, res) => {
+  try {
+    // Recoger id por la url
+    let id = req.params.id;
+    // Buscar articulo
+    let articulo = await Articulo.findById(id);
 
-  // Buscar artículo
-  Articulo.findById(id)
-    .then((articulo) => {
-      // Si no existe, devolver error
-      if (!articulo) {
-        return res.status(400).json({
-          status: "Error",
-          mensaje: "No se ha encontrado el artículo",
+    // Devolver resultado
+    return res.status(200).json({
+      status: "success",
+      articulo,
+    });
+  } catch (error) {
+    // Si no existe articulo devuelve esto
+    return res.status(400).json({
+      status: "error",
+      message: "Un error ha ocurrido mientras se obtenia el articulo",
+    });
+  }
+};
+// listar articulos
+
+const listar = (req, res) => {
+  Articulo.find({})
+
+    .then((articulos) => {
+      if (!articulos) {
+        return res.status(404).json({
+          status: "error",
+
+          mensaje: "No se han encontrado articulos",
         });
       }
 
-      // Devolver resultado
-      return res.status(200).json({
-        status: "Success",
-        articulo,
+      return res.status(200).send({
+        status: "success",
+
+        articulos,
       });
     })
+
     .catch((error) => {
-      return res.status(400).json({
-        status: "Error",
-        mensaje: "Ha ocurrido un error al buscar el artículo",
+      return res.status(500).json({
+        status: "error",
+
+        mensaje: "Ha ocurrido un error al listar los articulos",
+
+        error: error.message,
       });
     });
 };
-module.exports = { test, curso, crear, uno };
+
+// Borrar articulo
+const borrar = (req, res) => {
+  const id = req.params.id;
+
+  Articulo.findOneAndDelete({ _id: id }) // Usamos _id como filtro
+    .then((articuloEliminado) => {
+      if (!articuloEliminado) {
+        return res.status(404).json({
+          status: "Error",
+          message: "Articulo no encontrado",
+        });
+      }
+      return res.status(200).json({
+        status: "Success",
+        message: "Articulo eliminado con éxito",
+      });
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        status: "Error",
+        message: "Error al eliminar el artículo: " + error.message,
+      });
+    });
+};
+const editar = (req, res) => {
+  // Recoger id del cliente a editar
+  let editarId = req.params.id;
+  // Recoger datos del body
+  let parametros = req.body;
+  // Validamos datos
+  try {
+    let validar_titulo = !validator.isEmpty(parametros.titulo);
+    let validar_contenido = !validator.isEmpty(parametros.contenido);
+    if (!validar_titulo || !validar_contenido) {
+      throw new Error("No se ha validado la info");
+    }
+  } catch (error) {
+    return res.status(400).json({
+      status: "error",
+      mensaje: "Faltan datos por enviar",
+    });
+  }
+  // Buscar y actualizar el articulo
+  Articulo.findOneAndUpdate({ _id: editarId }, req.body, { new: true })
+    .then((clienteActualizado) => {
+      if (!clienteActualizado) {
+        return res.status(500).json({
+          status: "error",
+          mensaje: "Error al actualizar",
+        });
+      }
+      // Devolver respuesta
+      return res.status(200).json({
+        status: "success",
+        cliente: clienteActualizado,
+      });
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        status: "error",
+        mensaje: "Error al actualizar",
+      });
+    });
+};
+module.exports = { test, curso, crear, uno, listar, borrar, editar };
--- rutas/Articulo.js
+++ rutas/Articulo.js
@@ -7,7 +7,9 @@ router.get("/ruta-de-prueba", ArticuloController.test);
 router.get("/curso", ArticuloController.curso);
 
 router.post("/crear", ArticuloController.crear);
-router.get("/articulos", ArticuloController.listar);
 router.get("/articulo/:id", ArticuloController.uno);
+router.get("/articulos", ArticuloController.listar);
+router.delete("/articulo/:id", ArticuloController.borrar);
+router.put("/articulo/:id", ArticuloController.editar);
 
 module.exports = router;
